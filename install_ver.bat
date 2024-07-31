d895bd53748ebb36680ec2b5787733728b5eeffa
killcerr <93478098+killcerr@users.noreply.github.com>
2024-07-31 18:52:29 +0800

    fix: remove test comment

--- install_ver.bat
+++ install_ver.bat
@@ -1,11 +1,11 @@
-@REM powershell Invoke-WebRequest -o levilamina-release-nonmimalloc-windows-x64.zip https://github.com/CodeBedrock/levilamina-endstone/releases/download/v%1/levilamina-release-nonmimalloc-windows-x64.zip
-@REM powershell Invoke-WebRequest -o endstone-cp312-cp312-win_amd64.zip https://github.com/CodeBedrock/endstone-levilamina/releases/download/v%2/endstone-%2-cp312-cp312-win_amd64.whl
-@REM powershell Invoke-WebRequest -o cmdfix-windows-x64.zip https://github.com/CodeBedrock/cmdfix/releases/download/v%3/cmdfix-windows-x64.zip
-@REM powershell Invoke-WebRequest -o lip-windows-amd64.zip https://github.com/futrime/lip/releases/download/v0.23.1/lip-windows-amd64.zip
-@REM powershell -Command "Expand-Archive -LiteralPath levilamina-release-nonmimalloc-windows-x64.zip"
-@REM powershell -Command "Expand-Archive -LiteralPath endstone-cp312-cp312-win_amd64.zip"
-@REM powershell -Command "Expand-Archive -LiteralPath cmdfix-windows-x64.zip"
-@REM powershell -Command "Expand-Archive -LiteralPath lip-windows-amd64.zip"
+powershell Invoke-WebRequest -o levilamina-release-nonmimalloc-windows-x64.zip https://github.com/CodeBedrock/levilamina-endstone/releases/download/v%1/levilamina-release-nonmimalloc-windows-x64.zip
+powershell Invoke-WebRequest -o endstone-cp312-cp312-win_amd64.zip https://github.com/CodeBedrock/endstone-levilamina/releases/download/v%2/endstone-%2-cp312-cp312-win_amd64.whl
+powershell Invoke-WebRequest -o cmdfix-windows-x64.zip https://github.com/CodeBedrock/cmdfix/releases/download/v%3/cmdfix-windows-x64.zip
+powershell Invoke-WebRequest -o lip-windows-amd64.zip https://github.com/futrime/lip/releases/download/v0.23.1/lip-windows-amd64.zip
+powershell -Command "Expand-Archive -LiteralPath levilamina-release-nonmimalloc-windows-x64.zip"
+powershell -Command "Expand-Archive -LiteralPath endstone-cp312-cp312-win_amd64.zip"
+powershell -Command "Expand-Archive -LiteralPath cmdfix-windows-x64.zip"
+powershell -Command "Expand-Archive -LiteralPath lip-windows-amd64.zip"
 mkdir server
 cd server
 ..\lip-windows-amd64\lip install -y github.com/LiteLDev/LeviLamina
